191ff0b87aa4d46026d45443c2d42a8c132b06f8
TRIPTI <triptisamal20@gmail.com>
2021-10-22 00:45:35 -0400

    move sample files to correct directory

--- sample/projection.py
+++ sample/projection.py
@@ -1,50 +1,18 @@
+# import numpy to perform operations on vector
 import numpy as np
-import math
 
-def mag(x):
-    return math.sqrt(sum(i*i for i in x))
+u = np.array([1, 2, 3]) # vector u
+v = np.array([5, 6, 2]) # vector v:
 
+# Task: Project vector u on vector v
 
+# finding norm of the vector v
+v_norm = np.sqrt(sum(v**2))	
 
-packet = {
-            'dLoc':(3,4,5),   #destination location
-            'sLoc':(2,2,7) #source location
-           # 'myLoc':(2,6,4) #my location 
-        }#message details
-
-print(packet['dLoc'])
-print(packet['sLoc'])
-ds = tuple(map(lambda i, j: i - j, packet['sLoc'], packet['dLoc'] ))
-print(ds)
-
-myLoc = (2,6,4)
-#directional vector
-dt = tuple(map(lambda i, j: i - j, myLoc, packet['dLoc'] ))
-print(dt)
-
-#To find: projection of vector dt_v on ds_v
-
-dt_v = np.array([dt[0],dt[1],dt[2]])
-print(dt_v)
-ds_v = np.array([ds[0],ds[1],ds[2]])
-print(ds_v)
-
-# finding norm of the vector ds_v
-ds_v_norm = np.sqrt(sum(ds_v*ds_v)) 
-
-# Apply the formula for projecting a vector onto another vector
+# Apply the formula as mentioned above
+# for projecting a vector onto another vector
 # find dot product using np.dot()
-proj_of_dsv_on_dtv = (np.dot(dt_v, ds_v)/ds_v_norm*ds_v_norm)*ds_v
-print("Projection of Vector ds_v on Vector dt_v is: ", proj_of_dsv_on_dtv)
+proj_of_u_on_v = (np.dot(u, v)/v_norm**2)*v
 
-
-
-tb1 = (0.005 * mag(proj_of_dsv_on_dtv))/mag(ds_v)
-
-print(tb1)
-  
-
-
-
-
+print("Projection of Vector u on Vector v is: ", proj_of_u_on_v)
 
